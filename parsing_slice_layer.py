d9344b319b74cf827a099c22cdd1cb961f0fe9c2
hwang3 <972961854@qq.com>
2017-07-23 15:32:48 +0800

    update and fix bugs for adding slice layer to caffe model

--- parsing_slice_layer.py
+++ parsing_slice_layer.py
@@ -4,9 +4,10 @@ import argparse
 import re
 import json
 
+
 def arg_parse():
 	parser = argparse.ArgumentParser(description='Argument Parser for mxnet2caffe model parsing')
-	parser.add_argument('--filename', help='the name of the file which constain the id', 
+	parser.add_argument('-f', '--filename', help='the name of the file which constain the id', 
 							default='testlist.prototxt', type=str)
 	args = parser.parse_args()
 	return args
@@ -38,7 +39,18 @@ def get_slice_layers(jsons):
 	slice_candidates = [[y for y in jsons if y['bottom'][0]==x] for x in bottoms]
 	return list(map(lambda x: construct_slice_layer(x),slice_candidates))
 
-def remove_slice_json(filename):
+def write_slice_layer(slice_layers, bottom, writer):
+	# find the slice layer with target bottom
+	layers = list(filter(lambda x : x['bottom'] == bottom, slice_layers))
+	if len(layers):
+		layer = layers[0]
+		write_one_slice_layer(writer, layer)
+		slice_layers.remove(layer)
+	else:
+		pass
+	return slice_layers
+
+def replace_slice_json(filename, slice_layers):
 	temp = filename.replace('.prototxt','') + '_sliced.prototxt'
 	write_flag = True
 	with open(filename, 'r') as reader:
@@ -47,36 +59,40 @@ def remove_slice_json(filename):
 			while line:
 				if line == '<slice_json>\n':
 					write_flag = False
-				elif line == '</slice_json>\n':
-					write_flag = True
 				if write_flag:
 					writer.write(line)
+				else:
+					# get the bottom info
+					bottom = re.findall(r"'bottom': \['(.+?)'\]", line)
+					if (len(bottom)):
+						slice_layers = write_slice_layer(slice_layers, bottom[0], writer)
+				if line == '</slice_json>\n':
+					write_flag = True
 				line = reader.readline()
 	return temp
 
-def append_slice_layer(filename, slice_layers):
-	with open(filename, 'a') as writer:
-		for layer in slice_layers:
-			writer.write('\n')
-			writer.write('layer {\n')
-			writer.write('  bottom: "%s"\n' % layer['bottom'])
-			writer.write('  name: "%s"\n'   % layer['name'])
-			writer.write('  type: "Slice"\n')
-			for top in layer['tops']:
-				writer.write('  top: "%s"\n' % top)
-			writer.write('  slice_param {\n')
-			writer.write('  	axis: %s\n' % layer['axis'])
-			for split_point in layer['split_point']:
-				writer.write('  	slice_point: %s\n' % split_point)
-			writer.write('  }\n')
-			writer.write('}\n')
+def write_one_slice_layer(writer, layer):
+	writer.write('\n')
+	writer.write('layer {\n')
+	writer.write('  bottom: "%s"\n' % layer['bottom'])
+	writer.write('  name: "%s"\n'   % layer['name'])
+	writer.write('  type: "Slice"\n')
+	for top in layer['tops']:
+		writer.write('  top: "%s"\n' % top)
+	writer.write('  slice_param {\n')
+	writer.write('  	axis: %s\n' % layer['axis'])
+	for split_point in layer['split_point']:
+		writer.write('  	slice_point: %s\n' % split_point)
+	writer.write('  }\n')
+	writer.write('}\n')
+	writer.write('\n')
 
 # interface for in-python call
 def handle_slice_layers(filename):
 	jsons = parse_file(args.filename)
 	slice_layers = get_slice_layers(jsons)
-	newfile = remove_slice_json(args.filename)
-	append_slice_layer(newfile, slice_layers)
+	newfile = replace_slice_json(args.filename, slice_layers)
+	# append_slice_layer(newfile, slice_layers)
 	return newfile
 
 # independent call
@@ -84,5 +100,5 @@ if __name__ == '__main__':
 	args = arg_parse()
 	jsons = parse_file(args.filename)
 	slice_layers = get_slice_layers(jsons)
-	newfile = remove_slice_json(args.filename)
-	append_slice_layer(newfile, slice_layers)
+	newfile = replace_slice_json(args.filename, slice_layers)
+	# append_slice_layer(newfile, slice_layers)
